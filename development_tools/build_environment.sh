3ae22b347df209aebd842cd02fe797db7b6cd08e
Maxime Haselbauer <maxime.haselbauer@googlemail.com>
2026-01-28 09:22:43 +0100

    Create a base image for development environments

--- development_tools/build_environment.sh
+++ development_tools/build_environment.sh
@@ -2,12 +2,12 @@
 set -euxo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DEV_ENV_DEFINITION_SUBDIR="$DIR"/development_environments
-PREFIX="robot"
+PREFIX="ghcr.io/renn0xtek9/robot"
 # Build docker image locally
+docker build -t "$PREFIX"_base_image:0.0.1 "$DEV_ENV_DEFINITION_SUBDIR"/base_image
+docker push "$PREFIX"_base_image:0.0.1
+
 docker build -t "$PREFIX"_continuous_integration_environment:0.0.1 "$DEV_ENV_DEFINITION_SUBDIR"/continuous_integration_environment
 docker build -t "$PREFIX"_software_developer_environment:0.0.1 "$DEV_ENV_DEFINITION_SUBDIR"/software_developer_environment
 docker build -t "$PREFIX"_system_engineering_environment:0.0.1 "$DEV_ENV_DEFINITION_SUBDIR"/system_engineering_environment
-# Tag docker image 
-docker tag "$PREFIX"_system_engineering_environment:0.0.1 ghcr.io/renn0xtek9/"$PREFIX"_system_engineering_environment:0.0.1
-# Push docker images to gihub registry
-docker push ghcr.io/renn0xtek9/"$PREFIX"_system_engineering_environment:0.0.1
+docker push "$PREFIX"_system_engineering_environment:0.0.1
