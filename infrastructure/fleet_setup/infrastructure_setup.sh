f2b90ca334fdfff61c4164259f0c1b6ae9bab969
Maxime Haselbauer <maxime.haselbauer@googlemail.com>
2026-03-03 19:08:02 +0000

    Split ansible script that setup CI into download and activation

--- infrastructure/fleet_setup/infrastructure_setup.sh
+++ infrastructure/fleet_setup/infrastructure_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR" || exit 1
-ansible-playbook -i inventory.yaml continuous_integration/charlieindia.yaml -e "target_host=charlieindia1" --ask-pass --ask-become-pass
+echo "Enter GitHub Runner Token"
+read GH_RUNNER_TOKEN
+echo "Expected SHA256 of github runner package"
+read SHA256 
+ansible-playbook -i inventory.yaml continuous_integration/charlieindia_setup_runner.yaml -e "target_host=charlieindia1" -e "runner_token=$GH_RUNNER_TOKEN" -e "sha256_github_runner=$SHA256" --ask-pass --ask-become-pass
+ansible-playbook -i inventory.yaml continuous_integration/charlieindia_activate_runner.yaml -e "target_host=charlieindia1" --ask-pass --ask-become-pass
